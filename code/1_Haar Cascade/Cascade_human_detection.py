59b08652ef82fbcc6bd129802e01e119d4ed984a
applepiiie <47917321+applepiiie@users.noreply.github.com>
2019-02-23 20:04:31 +0800

    Add files via upload

---
+++ code/1_Haar Cascade/Cascade_human_detection.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+import time
+
+person_cascade = cv2.CascadeClassifier(
+    os.path.join(r'C:\Users\Mic\Desktop\Innovation Center\Test\1_Haar Cascade\haarcascade_fullbody.xml'))
+cap = cv2.VideoCapture(r"C:\Users\Mic\Desktop\Innovation Center\Test\Test Video.mp4")
+while True:
+    r, frame = cap.read()
+    if r:
+        start_time = time.time()
+        frame = cv2.resize(frame,(640,360)) # Downscale to improve frame rate
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # Haar-cascade classifier needs a grayscale image
+        rects = person_cascade.detectMultiScale(gray_frame)
+        
+        end_time = time.time()
+        print("Elapsed Time:",end_time-start_time)
+            
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame, (x,y), (x+w,y+h),(0,255,0),2)
+        cv2.imshow("preview", frame)
+    k = cv2.waitKey(1)
+    if k & 0xFF == ord("q"): # Exit condition
+        break
